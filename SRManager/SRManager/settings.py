10ccfc37e263928492017004ced200ec45f99548
goutham <30994768+MGouthamB@users.noreply.github.com>
2024-08-23 09:52:40 -0400

    static update

--- SRManager/SRManager/settings.py
+++ SRManager/SRManager/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"issi_static")
 
 AUTH_USER_MODEL="reports.CustomUser"
 
